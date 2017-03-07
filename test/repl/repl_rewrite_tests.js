6518fc0febd6d7106995e17822fe7e4b9c972908
Matt Broadstone <mbroadst@gmail.com>
2017-03-07 11:48:28 -0500

    refactor(rewrite-tests): greatly simply the tests for easy addition

--- test/repl/repl_rewrite_tests.js
+++ test/repl/repl_rewrite_tests.js
@@ -3,143 +3,122 @@ const assert = require('assert'),
       rewriteScript = require('../../lib/executor').rewriteScript,
       preprocess = require('../../lib/repl').preprocess;
 
+function describeEx(desc, fn, tests) {
+  describe(desc, function() {
+    tests.forEach(test => {
+      it(test.name, function() {
+        let actual = fn(test.input);
+        assert.equal(actual, test.expected);
+      });
+    });
+  });
+}
+
 describe('Repl Rewrite Tests', function() {
-  describe('preprocess', function() {
-    it('should wrap async assignments with a wrapper for immediate execution', function() {
-      let input = 'x = await t.find(querySpec).count();';
-      let expected = '(() => { async function _wrap() { return global.x = await t.find(querySpec).count(); } return _wrap(); })();';
-      let actual = preprocess(input);
-      assert.equal(actual, expected);
-    });
+  describeEx('preprocess', preprocess, [
+    {
+      name: 'should wrap async assignments with a wrapper for immediate execution',
+      input: 'x = await t.find(querySpec).count();',
+      expected: '(() => { async function _wrap() { return global.x = await t.find(querySpec).count(); } return _wrap(); })();'
+    },
+    {
+      name: 'should wrap async method args with a wrapper for imemdiate execution (1)',
+      input: 'assert.writeOK(await t.mycoll.insert({}));',
+      expected: '(() => { async function _wrap() { return assert.writeOK(await t.mycoll.insert({})); } return _wrap(); })();'
+    },
+    {
+      name: 'should wrap async method args with a wrapper for imemdiate execution (2)',
+      input: 'assert.writeOK(10, await t.mycoll.insert({}));',
+      expected: '(() => { async function _wrap() { return assert.writeOK(10, await t.mycoll.insert({})); } return _wrap(); })();'
+    }
+  ]);
 
-    it('should wrap async method args with a wrapper for imemdiate execution (1)', function() {
-      let input = 'assert.writeOK(await t.mycoll.insert({}));';
-      let expected = '(() => { async function _wrap() { return assert.writeOK(await t.mycoll.insert({})); } return _wrap(); })();';
-      let actual = preprocess(input);
-      assert.equal(actual, expected);
-    });
+  describeEx('async arguments', rewriteScript, [
+    {
+      name: 'should not prepend await to a sync method containing an async argument',
+      input: "assert.eq(cursor.next(), {_id: 1, strs: ['2000', '60']});",
+      expected: "assert.eq(await cursor.next(), {_id: 1, strs: ['2000', '60']});"
+    },
+    {
+      name: 'should not prepend await to a sync method containing multiple async arguments',
+      input: 'assert.eq(cursor.next(), cursor.next());',
+      expected: 'assert.eq(await cursor.next(), await cursor.next());'
+    }
+  ]);
 
-    it('should wrap async method args with a wrapper for imemdiate execution (2)', function() {
-      let input = 'assert.writeOK(10, await t.mycoll.insert({}));';
-      let expected = '(() => { async function _wrap() { return assert.writeOK(10, await t.mycoll.insert({})); } return _wrap(); })();';
-      let actual = preprocess(input);
-      assert.equal(actual, expected);
-    });
-  });
+  describeEx('assert.throws', rewriteScript, [
+    {
+      name: 'should rewrite async methods in `assert.throws` to use async form',
+      input: 'assert.throws(function() { t.find({$and: 4}).toArray(); });',
+      expected: 'await assert.throws(async function() { await t.find({$and: 4}).toArray(); });'
+    },
+    {
+      name: 'should rewrite async methods in `assert.throws` to use async form within a function',
+      input: 'function check() { assert.throws(function() { t.find({$and: 4}).toArray(); }); }',
+      expected: 'async function check() { await assert.throws(async function() { await t.find({$and: 4}).toArray(); }); }'
+    }
+  ]);
 
-  it('should rewrite an async method to a generator with await', function() {
-    let input = 'function test() { t.find(querySpec).sort(sortSpec).batchSize(1000).count(); }';
-    let expected = 'async function test() { await t.find(querySpec).sort(sortSpec).batchSize(1000).count(); }';
-    let actual = rewriteScript(input);
-    assert.equal(actual, expected);
-  });
-
-  it('should rewrite an IIFE to await and accept a generator', function() {
-    let input = '(function() { var coll = db.sort1; coll.findOne({}); })();';
-    let expected = 'await (async function() { var coll = db.sort1; await coll.findOne({}); })();';
-    let actual = rewriteScript(input);
-    assert.equal(actual, expected);
-  });
-
-  it('should only prepend a single await for a function containing an async method (1)', function() {
-    let input = '(function() { var coll = db.sort1; coll.findOne({}); coll.findOne({}); })();';
-    let expected = 'await (async function() { var coll = db.sort1; await coll.findOne({}); await coll.findOne({}); })();';
-    let actual = rewriteScript(input);
-    assert.equal(actual, expected);
-  });
-
-  describe('async arguments', function() {
-    it('should not prepend await to a sync method containing an async argument', function() {
-      let input = "assert.eq(cursor.next(), {_id: 1, strs: ['2000', '60']});";
-      let expected = "assert.eq(await cursor.next(), {_id: 1, strs: ['2000', '60']});";
-      let actual = rewriteScript(input);
-      assert.equal(actual, expected);
-    });
-
-    it('should not prepend await to a sync method containing multiple async arguments', function() {
-      let input = 'assert.eq(cursor.next(), cursor.next());';
-      let expected = 'assert.eq(await cursor.next(), await cursor.next());';
-      let actual = rewriteScript(input);
-      assert.equal(actual, expected);
-    });
-  });
-
-  it('should wrap awaited methods beginning with `!`', function() {
-    let input = 'assert(!cursor.hasNext());';
-    let expected = 'assert(!(await cursor.hasNext()));';
-    let actual = rewriteScript(input);
-    assert.equal(actual, expected);
-  });
-
-  it('should not add additional awaits to a term thats already been awaited', function() {
-    let input = 'const results = find(options.query).toArray();';
-    let expected = 'const results = await find(options.query).toArray();';
-    let actual = rewriteScript(input);
-    assert.equal(actual, expected);
-  });
-
-  it('should await future references to converted async method', function() {
-    let input = 'function test() { return db.coll.findOne({}); }; test();';
-    let expected = 'async function test() { return await db.coll.findOne({}); }; await test();';
-    let actual = rewriteScript(input);
-    assert.equal(actual, expected);
-  });
-
-  it('should wrap awaited async methods when properties are accessed in the resulting value', function() {
-    let input = 'function test() { return db.coll.findOne({}).x; };';
-    let expected = 'async function test() { return (await db.coll.findOne({})).x; };';
-    let actual = rewriteScript(input);
-    assert.equal(actual, expected);
-  });
-
-  it('should wrap again better description', function() {
-    let input = 'assert.eq(1, t.find({$where: "return this.a == 2"}).toArray().length, "B");';
-    let expected = 'assert.eq(1, (await t.find({$where: "return this.a == 2"}).toArray()).length, "B");';
-    let actual = rewriteScript(input);
-    assert.equal(actual, expected);
-  });
-
-  it('should properly wrap async methods when used with an `in` operator', function() {
-    let input = "assert(!('zeroPad' in col.findOne({_id: result.insertedId})));";
-    let expected = "assert(!('zeroPad' in (await col.findOne({_id: result.insertedId}))));";
-    let actual = rewriteScript(input);
-    assert.equal(actual, expected);
-  });
-
-  it('should await async methods that are assigned to a variable', function() {
-    let input = 'doTest = function() { t.findOne({}); }; doTest();';
-    let expected = 'doTest = async function() { await t.findOne({}); }; await doTest();';
-    let actual = rewriteScript(input);
-    assert.equal(actual, expected);
-  });
-
-  describe('assert.throws', function() {
-    it('should rewrite async methods in `assert.throws` to use async form', function() {
-      let input = 'assert.throws(function() { t.find({$and: 4}).toArray(); });';
-      let expected = 'await assert.throws(async function() { await t.find({$and: 4}).toArray(); });';
-      let actual = rewriteScript(input);
-      assert.equal(actual, expected);
-    });
-
-    it('should rewrite async methods in `assert.throws` to use async form within a function', function() {
-      let input = 'function check() { assert.throws(function() { t.find({$and: 4}).toArray(); }); }';
-      let expected = 'async function check() { await assert.throws(async function() { await t.find({$and: 4}).toArray(); }); }';
-      let actual = rewriteScript(input);
-      assert.equal(actual, expected);
-    });
-  });
-
-  it('should wrap an async call with parens if subsequent calls on the object are not async', function() {
-    let input = 'db.getCollectionNames().forEach(function(x) {});';
-    let expected = '(await db.getCollectionNames()).forEach(function(x) {});';
-    let actual = rewriteScript(input);
-    assert.equal(actual, expected);
-  });
-
-  it('should not wrap explain with async decorations', function() {
-    let input = 'db.basic_test_1.explain().find({});';
-    let expected = 'db.basic_test_1.explain().find({});';
-    let actual = rewriteScript(input);
-    assert.equal(actual, expected);
-  });
+  describeEx('basic', rewriteScript, [
+    {
+      name: 'should rewrite an async method to a generator with await',
+      input: 'function test() { t.find(querySpec).sort(sortSpec).batchSize(1000).count(); }',
+      expected: 'async function test() { await t.find(querySpec).sort(sortSpec).batchSize(1000).count(); }'
+    },
+    {
+      name: 'should rewrite an IIFE to await and accept a generator',
+      input: '(function() { var coll = db.sort1; coll.findOne({}); })();',
+      expected: 'await (async function() { var coll = db.sort1; await coll.findOne({}); })();'
+    },
+    {
+      name: 'should only prepend a single await for a function containing an async method (1)',
+      input: '(function() { var coll = db.sort1; coll.findOne({}); coll.findOne({}); })();',
+      expected: 'await (async function() { var coll = db.sort1; await coll.findOne({}); await coll.findOne({}); })();'
+    },
+    {
+      name: 'should wrap awaited methods beginning with `!`',
+      input: 'assert(!cursor.hasNext());',
+      expected: 'assert(!(await cursor.hasNext()));'
+    },
+    {
+      name: 'should not add additional awaits to a term thats already been awaited',
+      input: 'const results = find(options.query).toArray();',
+      expected: 'const results = await find(options.query).toArray();'
+    },
+    {
+      name: 'should await future references to converted async method',
+      input: 'function test() { return db.coll.findOne({}); }; test();',
+      expected: 'async function test() { return await db.coll.findOne({}); }; await test();'
+    },
+    {
+      name: 'should wrap awaited async methods when properties are accessed in the resulting value',
+      input: 'function test() { return db.coll.findOne({}).x; };',
+      expected: 'async function test() { return (await db.coll.findOne({})).x; };'
+    },
+    {
+      name: 'should wrap again better description',
+      input: 'assert.eq(1, t.find({$where: "return this.a == 2"}).toArray().length, "B");',
+      expected: 'assert.eq(1, (await t.find({$where: "return this.a == 2"}).toArray()).length, "B");'
+    },
+    {
+      name: 'should properly wrap async methods when used with an `in` operator',
+      input: "assert(!('zeroPad' in col.findOne({_id: result.insertedId})));",
+      expected: "assert(!('zeroPad' in (await col.findOne({_id: result.insertedId}))));"
+    },
+    {
+      name: 'should await async methods that are assigned to a variable',
+      input: 'doTest = function() { t.findOne({}); }; doTest();',
+      expected: 'doTest = async function() { await t.findOne({}); }; await doTest();'
+    },
+    {
+      name: 'should wrap an async call with parens if subsequent calls on the object are not async',
+      input: 'db.getCollectionNames().forEach(function(x) {});',
+      expected: '(await db.getCollectionNames()).forEach(function(x) {});'
+    },
+    {
+      name: 'should not wrap explain with async decorations',
+      input: 'db.basic_test_1.explain().find({});',
+      expected: 'db.basic_test_1.explain().find({});'
+    }
+  ]);
 });
