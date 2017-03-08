c3551071e1daa176f4454c3f648b4087c7b17aff
Matt Broadstone <mbroadst@gmail.com>
2017-03-08 15:26:52 -0500

    refactor(repl_basic_tests): remove `co` dependency from test suite

--- test/repl/repl_basic_tests.js
+++ test/repl/repl_basic_tests.js
@@ -1,71 +1,47 @@
+'use strict';
 const ReplTestFixture = require('./repl_test_fixture'),
-      co = require('co'),
-      rmdir = require('rmdir'),
       assert = require('assert');
+
+function executeRepl(test, string, context) {
+  return new Promise((resolve, reject) => {
+    test.repl.eval(string, context, '', (err, result) => {
+      if (err) {
+        return reject(new SyntaxError(err));
+      }
+
+      resolve(result);
+    });
+  });
+}
 
 let test = new ReplTestFixture();
 before(() => test.databaseSetup());
 after(() => test.databaseTeardown());
 
-describe('Repl Basic tests', () => {
-  describe('basic operations', () => {
+describe('Repl Basic tests', function() {
+  describe('basic operations', function() {
     beforeEach(() => test.setup());
 
-    it('should correctly return results for find()', function(done) {
-      co(function*() {
-        // Insert a document
-        let result = yield executeRepl(test, 'db.basic_test_1.insertOne({a:1})', test.context);
-
-        // Get the document
-        result = JSON.parse(yield executeRepl(test, 'db.basic_test_1.find({})', test.context));
-        assert.equal(1, result.a);
-        done();
-      }).catch(err => {
-        console.log(err);
-        throw err;
-      });
+    it('should correctly return results for find()', async function() {
+      await executeRepl(test, 'db.basic_test_1.insertOne({a:1})', test.context);
+      let result = await executeRepl(test, 'db.basic_test_1.find({})', test.context);
+      result = JSON.parse(result);
+      assert.equal(1, result.a);
     });
 
-    it('should correctly explain find()', function(done) {
-      co(function*() {
-        // Insert a document
-        let result = yield executeRepl(test, 'db.basic_test_2.insertOne({a:1})', test.context);
-
-        // Get the document
-        result = yield executeRepl(test, 'db.basic_test_1.explain().find({})', test.context);
-        console.log(result)
-
-        done();
-      }).catch(err => {
-        console.log(err);
-        throw err;
-      });
+    it('should correctly explain find()', async function() {
+      await executeRepl(test, 'db.basic_test_2.insertOne({a:1})', test.context);
+      let result = await executeRepl(test, 'db.basic_test_1.explain().find({})', test.context);
+      console.log(result);
     });
 
-    it('should correctly execute for loop with insertOne', function(done) {
-      co(function*() {
-        // Insert a document
-        let result = yield executeRepl(test, 'for(var i = 0; i < 100; i++) db.basic_test_3.insertOne({a:i})', test.context);
-        console.log(result)
-        //
-        // // Get the document
-        // result = yield executeRepl(test, 'db.basic_test_1.explain().find({})', test.context);
+    it('should correctly execute for loop with insertOne', async function() {
+      let result = await executeRepl(test, 'for(var i = 0; i < 100; i++) db.basic_test_3.insertOne({a:i})', test.context);
+      console.log(result);
 
-        done();
-      }).catch(err => {
-        console.log(err);
-        throw err;
-      });
+      // // Get the document
+      // result = yield executeRepl(test, 'db.basic_test_1.explain().find({})', test.context);
     });
   });
 });
 
-function executeRepl(test, string, context) {
-  return new Promise((resolve, reject) => {
-    // Execute command
-    test.repl.eval(string, context, '', (err, result) => {
-      if(err) return reject(err);
-      resolve(result);
-    });
-  });
-}
